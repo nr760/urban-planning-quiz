eb921da3b2b47586c4224b43927319e9d7408bbd
Nithil <nr760@snu.edu.in>
2026-09-18 21:18:13 +0530

    Add questions for urban development quiz

---
+++ questions.js
@@ -0,0 +1,98 @@
+const questions = [
+
+{
+question:"As per Census of India 2011 classification, towns with populations between 5,000 and 49,999 fall under Class V, IV and III towns.",
+options:["True","False"],
+answer:"True"
+},
+
+{
+question:"Which of the following is a criterion for identifying a Census Town in India?",
+options:[
+"Minimum population of 10000",
+"Minimum population of 5000",
+"Minimum population of 1000",
+"Minimum population of 50000"
+],
+answer:"Minimum population of 5000"
+},
+
+{
+question:"Urbanization as a demographic process means:",
+options:[
+"Decline in urban dwellers",
+"Increase in percentage of population residing in urban areas",
+"Decrease in city size",
+"Disappearance of rural settlements"
+],
+answer:"Increase in percentage of population residing in urban areas"
+},
+
+{
+question:"Article 243ZE provides for Metropolitan Planning Committee.",
+options:["True","False"],
+answer:"True"
+},
+
+{
+question:"Class I towns have population:",
+options:[
+"Below 50000",
+"50000-100000",
+"100000 and above",
+"Above 500000"
+],
+answer:"100000 and above"
+},
+
+{
+question:"Major components of urban growth are:",
+options:[
+"Natural increase, migration and reclassification",
+"Deforestation and rainfall",
+"Agriculture and forestry",
+"Income and literacy"
+],
+answer:"Natural increase, migration and reclassification"
+},
+
+{
+question:"Nagar Panchayat is constituted for:",
+options:[
+"Metropolitan area",
+"Area in transition from rural to urban",
+"District level planning",
+"Village cluster"
+],
+answer:"Area in transition from rural to urban"
+},
+
+{
+question:"Fisheries is included in the Eleventh Schedule.",
+options:["True","False"],
+answer:"True"
+},
+
+{
+question:"Transit-Oriented Development promotes:",
+options:[
+"Compact development near transit",
+"Urban sprawl",
+"Single-use industrial zoning",
+"Rural settlement dispersal"
+],
+answer:"Compact development near transit"
+},
+
+{
+question:"Consent required under RFCTLARR Act 2013 for private company project:",
+options:[
+"50%",
+"60%",
+"70%",
+"80%"
+],
+answer:"80%"
+}
+
+];
